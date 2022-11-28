2d84ccf4afe1ace4dc07f591cbf990bb24ebda93
JoanOFDark <87716444+JoanOFDark@users.noreply.github.com>
2022-11-28 21:35:13 +0300

    Add files via upload

---
+++ search-server/log_duration.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <string_view>
+
+#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+#define LOG_DURATION_STREAM(x, y) LogDuration UNIQUE_VAR_NAME_PROFILE(x, y)
+
+class LogDuration {
+public:
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(std::string_view id, std::ostream& dst_stream = std::cerr)
+        : id_(id)
+        , dst_stream_(dst_stream) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        dst_stream_ << id_ << ": "sv << duration_cast<milliseconds>(dur).count() << " ms"sv << std::endl;
+    }
+
+private:
+    const std::string id_;
+    const Clock::time_point start_time_ = Clock::now();
+    std::ostream& dst_stream_;
+};
+
+//#pragma once
+//
+//#include <chrono>
+//#include <iostream>
+//#include <string_view>
+//
+//using namespace std;
+//using namespace chrono;
+//using namespace literals;
+//
+//#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+//#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+//#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+//#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+//
+//class LogDuration {
+//public:
+//    LogDuration(const std::string& id) : id_(id) {
+//    }
+//
+//    ~LogDuration() {
+//        const auto end_time = steady_clock::now();
+//        const auto dur = end_time - start_time_;
+//        cerr << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << endl;
+//    }
+//
+//private:
+//    const std::string id_;
+//    const steady_clock::time_point start_time_ = steady_clock::now();
+//};
